6d8131e7d9ac1670d455bdaca2df89df9cbe9477
francescorestelli <francesco.restelli@roc-connect.com>
2020-02-22 20:48:09 +0100

    use fun

--- src/observer_cli.erl
+++ src/observer_cli.erl
@@ -22,7 +22,7 @@ start() -> start(#view_opts{}).
 -spec start(Node) -> no_return when Node :: atom() | non_neg_integer().
 start(Node) when Node =:= node() -> start(#view_opts{});
 start(Node) when is_atom(Node) -> rpc_start(Node, ?DEFAULT_INTERVAL);
-start(Pid) when is_pid(Pid) -> 	start(#view_opts{home=#home{owner=Pid}});
+start(PrintFun) when is_function(PrintFun) -> 	start(#view_opts{home=#home{printfun= PrintFun}});
 start(#view_opts{home = Home} = Opts) ->
     erlang:process_flag(trap_exit, true),
     AutoRow = check_auto_row(),
